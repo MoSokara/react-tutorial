a2304a796164a570cb10fd94624c620e68dc543e
Mohamed_Sokara <mosokara2007@gmail.com>
2026-05-21 23:46:45 +0300

    complete challenge: Object State

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
 import "./App.css";
-import Header from "./components/Header";
-import SideMenu from "./components/SideMenu";
-import Post from "./components/Post";
+import Scoreboard from "./components/Scoreboard";
 
 function App() {
-  const posts = [
-    {
-      id: 1,
-      title: "Post 1",
-      content: "This is the content of the first post.",
-    },
-    {
-      id: 2,
-      title: "Post 2",
-      content: "This is the content of the second post.",
-    },
-    {
-      id: 3,
-      title: "Post 3",
-      content: "This is the content of the third post.",
-    },
-    {
-      id: 4,
-      title: "Post 4",
-      content: "This is the content of the fourth post.",
-    },
-    {
-      id: 5,
-      title: "Post 5",
-      content: "This is the content of the fifth post.",
-    },
-  ];
-
-  const postRender = posts.map((item) => {
-    return <Post key={item.id} title={item.title} content={item.content} />;
-  });
-
   return (
     <div className="App">
-      <Header />
-      <main className="App-main">
-        <div className="posts-container">{postRender}</div>
-        <SideMenu />
-      </main>
+      <Scoreboard />
     </div>
   );
 }
---
+++ src/components/Scoreboard.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+export default function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: "Ranjani",
+    lastName: "Shettar",
+    score: 0,
+  });
+
+  function handlePlusClick() {
+    setPlayer({
+      ...player,
+      score: player.score + 1,
+    });
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value,
+    });
+  }
+
+  return (
+    <form
+      className="react-form-challenge"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <label>
+        Score: <b>{player.score}</b>{" "}
+        <button onClick={handlePlusClick}>+1</button>
+      </label>
+      <label>
+        First name:
+        <input value={player.firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name:
+        <input value={player.lastName} onChange={handleLastNameChange} />
+      </label>
+    </form>
+  );
+}
